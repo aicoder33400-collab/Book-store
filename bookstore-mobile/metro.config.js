9ed007ab7bea8b5847358c9ed587edbd061765b3
mahbaiz <mouhcine.ahbaiz@inria.fr>
2026-09-15 10:12:06 +0200

    fix: configuration Metro pour compatibilité web (zustand import.meta)

--- bookstore-mobile/metro.config.js
+++ bookstore-mobile/metro.config.js
@@ -4,4 +4,8 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'env'];
 
+// 🔥 Forcer Metro à utiliser la version CommonJS de zustand
+// (évite import.meta dans le bundle web)
+config.resolver.unstable_conditionNames = ['browser', 'require', 'react-native'];
+
 module.exports = config;
